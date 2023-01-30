16911593aadf60c29886500e3af7c80bfcd647cf
casey-SK <56776006+casey-SK@users.noreply.github.com>
2023-01-29 20:28:29 -0600

    double blinky example

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,43 @@
 #![feature(type_alias_impl_trait)]
 
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
+use embassy_rp::gpio::{self, Pin};
 use embassy_time::{Duration, Timer};
-use gpio::{Level, Output};
+use gpio::{AnyPin, Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_18, Level::Low);
 
+    let led_red: AnyPin = (p.PIN_2).degrade();
+    let led_green: AnyPin = (p.PIN_1).degrade();
+
+    let t1 = Duration::from_millis(1000);
+    let t2 = Duration::from_millis(350);
+
+    spawner.spawn(blinker_red(led_red, t1)).unwrap();
+    spawner.spawn(blinker_green(led_green, t2)).unwrap();
+}
+
+#[embassy_executor::task]
+async fn blinker_red(pin: AnyPin, interval: Duration) {
+    let mut led = Output::new(pin, Level::Low);
     loop {
         led.set_high();
-        Timer::after(Duration::from_millis(100)).await;
-
+        Timer::after(interval).await;
         led.set_low();
-        Timer::after(Duration::from_millis(100)).await;
+        Timer::after(interval).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn blinker_green(pin: AnyPin, interval: Duration) {
+    let mut led = Output::new(pin, Level::Low);
+    loop {
+        led.set_high();
+        Timer::after(interval).await;
+        led.set_low();
+        Timer::after(interval).await;
     }
 }
