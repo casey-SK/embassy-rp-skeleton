2e4ebf4f35ef20f8510489c2f87f42754acc62cd
casey-SK <56776006+casey-SK@users.noreply.github.com>
2023-01-29 18:42:10 -0600

    skeletonize the skeleton template

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::gpio;
 use embassy_time::{Duration, Timer};
@@ -11,30 +10,14 @@ use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    // Initialise Peripherals
     let p = embassy_rp::init(Default::default());
+    let mut led = Output::new(p.PIN_18, Level::Low);
 
-    // Create LED
-    let mut led = Output::new(p.PIN_25, Level::Low);
-
-    // Loop
     loop {
-        // Log
-        info!("LED On!");
-
-        // Turn LED On
         led.set_high();
-
-        // Wait 100ms
         Timer::after(Duration::from_millis(100)).await;
 
-        // Log
-        info!("LED Off!");
-
-        // Turn Led Off
         led.set_low();
-
-        // Wait 100ms
         Timer::after(Duration::from_millis(100)).await;
     }
 }
