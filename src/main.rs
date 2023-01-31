a5423dbbe3854068d2893128911aa3fe517114bb
casey-SK <56776006+casey-SK@users.noreply.github.com>
2023-01-31 00:15:56 -0600

    This working project has: - multitasking blinking LEDs - TMP36 sensor input - Signal between embassy tasks - USB Serial output of temperature data - A third LED which blinks when temperature is within a range

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,34 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+
 use embassy_executor::Spawner;
-use embassy_rp::adc::{Adc, Config};
-use embassy_rp::interrupt::{self, ADC_IRQ_FIFO};
-
-use embassy_rp::peripherals::PIN_28;
-use embassy_rp::peripherals::ADC;
-
-use embassy_rp::gpio::{self, Pin};
-use gpio::{AnyPin, Level, Output};
-
-use embassy_time::{Duration, Timer};
-use {defmt_rtt as _, panic_probe as _};
-
 use embassy_sync::signal::Signal;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_time::{Duration, Timer};
+use embassy_usb::{Builder, Config, UsbDevice, driver::EndpointError, class::cdc_acm::{CdcAcmClass, State}};
 
+use embassy_rp::adc::{self, Adc};
+use embassy_rp::interrupt::{self, ADC_IRQ_FIFO};
+use embassy_rp::peripherals::{PIN_28, ADC, USB};
+use embassy_rp::gpio::{Pin, AnyPin, Level, Output};
+use embassy_rp::usb::{Driver, Instance};
+
+use format_no_std::*;
+use static_cell::StaticCell;
+use defmt::{info, panic};
+use {defmt_rtt as _, panic_probe as _};
+
+
+type MyDriver = Driver<'static, USB>;
 static TEMPERATURE_SIGNAL: Signal<CriticalSectionRawMutex, f32> = Signal::new();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let irq = interrupt::take!(ADC_IRQ_FIFO);
-    //let mut adc = Adc::new(p.ADC, irq, Config::default());
-
+    let adc_irq = interrupt::take!(ADC_IRQ_FIFO);
+    let usb_irq = interrupt::take!(USBCTRL_IRQ);
+    let driver = Driver::new(p.USB, usb_irq);
     let adc_p = p.ADC;
 
     let led_red: AnyPin = (p.PIN_6).degrade();
@@ -37,16 +41,85 @@ async fn main(spawner: Spawner) {
     let led = (p.PIN_4).degrade();
     let p28 = p.PIN_28;
 
+
+    // Create embassy-usb Config
+    let mut config = Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-serial example");
+    config.serial_number = Some("12345678");
+    config.max_power = 100;
+    config.max_packet_size_0 = 64;
+
+    // Required for windows compatiblity.
+    // https://developer.nordicsemi.com/nRF_Connect_SDK/doc/1.9.1/kconfig/CONFIG_CDC_ACM_IAD.html#help
+    config.device_class = 0xEF;
+    config.device_sub_class = 0x02;
+    config.device_protocol = 0x01;
+    config.composite_with_iads = true;
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    struct Resources {
+        device_descriptor: [u8; 256],
+        config_descriptor: [u8; 256],
+        bos_descriptor: [u8; 256],
+        control_buf: [u8; 64],
+        serial_state: State<'static>,
+    }
+    static RESOURCES: StaticCell<Resources> = StaticCell::new();
+    let res = RESOURCES.init(Resources {
+        device_descriptor: [0; 256],
+        config_descriptor: [0; 256],
+        bos_descriptor: [0; 256],
+        control_buf: [0; 64],
+        serial_state: State::new(),
+    });
+
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut res.device_descriptor,
+        &mut res.config_descriptor,
+        &mut res.bos_descriptor,
+        &mut res.control_buf,
+        None,
+    );
+
+    // Create classes on the builder.
+    let class = CdcAcmClass::new(&mut builder, &mut res.serial_state, 64);
+
+    // Build the builder.
+    let usb = builder.build();
+
+    Timer::after(Duration::from_millis(1_000)).await;
+
     spawner.spawn(blinker(led_red, t1)).unwrap();
     spawner.spawn(blinker(led_green, t2)).unwrap();
-    spawner.spawn(read_temperature(p28, irq, adc_p)).unwrap();
+    spawner.spawn(read_temperature(p28, adc_irq, adc_p)).unwrap();
     spawner.spawn(indicate_temperature(led)).unwrap();
+    spawner.spawn(usb_task(usb)).unwrap();
+    spawner.spawn(echo_task(class)).unwrap();
 
 }
 
 #[embassy_executor::task]
+async fn usb_task(mut device: UsbDevice<'static, MyDriver>) {
+    device.run().await;
+}
+
+#[embassy_executor::task]
+async fn echo_task(mut class: CdcAcmClass<'static, MyDriver>) {
+    loop {
+        class.wait_connection().await;
+        info!("Connected");
+        let _ = echo(&mut class).await;
+        info!("Disconnected");
+    }
+}
+
+#[embassy_executor::task]
 async fn read_temperature(mut p28: PIN_28, irq: ADC_IRQ_FIFO, adc_p: ADC) {
-    let mut adc = Adc::new(adc_p, irq, Config::default());
+    let mut adc = Adc::new(adc_p, irq, adc::Config::default());
 
     loop {
         let level = adc.read(&mut p28).await;
@@ -110,4 +183,27 @@ fn scaling(raw_value: u16) -> f32 {
     let temperature = raw_tension as f32 * 175.0 / 1750.0 - 50.0;
 
     return temperature;
+}
+
+struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+async fn echo<'d, T: Instance + 'd>(class: &mut CdcAcmClass<'d, Driver<'d, T>>) -> Result<(), Disconnected> {
+    loop {
+        let temp = TEMPERATURE_SIGNAL.wait().await;
+
+        let mut buffer = [0u8;64];
+        let s: &str = show(&mut buffer, format_args!("Temperature: {:.2} °C\r\n", temp)).unwrap();
+
+        class.write_packet(s.as_bytes()).await?;
+        Timer::after(Duration::from_secs(2)).await;
+    }
 }
